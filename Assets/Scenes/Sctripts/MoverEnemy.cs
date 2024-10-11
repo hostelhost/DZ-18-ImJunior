8eadd7136811cd570f234d0a2be1c2fa7d48d9d8
Илья <Hostel_2008@mail.ru>
2024-10-11 19:34:47 +0300

    2D Платформер
    исправлены ошибки в enemy исправлены ошибки в mover player добавены все анимации player

--- Assets/Scenes/Sctripts/MoverEnemy.cs
+++ Assets/Scenes/Sctripts/MoverEnemy.cs
@@ -2,29 +2,29 @@ using UnityEngine;
 
 public class MoverEnemy : MonoBehaviour
 {
-    [SerializeField] private Point[] Targets;
-    [SerializeField] private float _speed = 2f;
+    [SerializeField] private Point[] _targets;
+    [SerializeField] private float _speed = 3f;
 
     private Vector3 _target;
-    private int _countTarget;
+    private int _currentIndexOfTarget;
 
-    private void Awake()
+    private void Start()
     {
-        _countTarget = Targets.Length-1;
+        _currentIndexOfTarget = _targets.Length - 1;
+        _target = NextTarget();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
 
         if (Vector2.SqrMagnitude(transform.position - _target) == 0 * 0)
-            _target = NextTarget();       
+            _target = NextTarget();
     }
 
     private Vector3 NextTarget()
     {
-        _countTarget = ++_countTarget % Targets.Length;
-
-        return Targets[_countTarget].transform.position;      
+        _currentIndexOfTarget = ++_currentIndexOfTarget % _targets.Length;
+        return _targets[_currentIndexOfTarget].transform.position;
     }
 }
--- Assets/Scenes/Sctripts/MoverPlayer.cs
+++ Assets/Scenes/Sctripts/MoverPlayer.cs
@@ -1,6 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoverPlayer : MonoBehaviour
@@ -8,14 +6,25 @@ public class MoverPlayer : MonoBehaviour
     private const string Horizontal = "Horizontal";
     private const string Vertical = "Vertical";
 
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _speed = 7.0f;
+
     private float _inputHorizontal;
     private float _inputVertical;
 
     private void Update()
     {
-        _inputHorizontal = Input.GetAxis(Horizontal);
-        _inputVertical = Input.GetAxis(Vertical);
+        _inputHorizontal = Input.GetAxisRaw(Horizontal);
+        _inputVertical = Input.GetAxisRaw(Vertical);
 
-        transform.Translate(_inputHorizontal * Time.deltaTime, _inputVertical * Time.deltaTime, 0, Space.World);
+        ManageAnimator();
+
+        transform.Translate(_inputHorizontal * Time.deltaTime * _speed, _inputVertical * Time.deltaTime * _speed, 0, Space.World);
+    }
+
+    private void ManageAnimator()
+    {
+        _animator.SetFloat("horizonalAxis", _inputHorizontal);
+        _animator.SetFloat("verticalAxis", _inputVertical);
     }
 }
